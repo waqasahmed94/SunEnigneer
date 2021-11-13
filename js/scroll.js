a5ea2923beb5d6f44d809fe0ad0a57a7450849a9
waqasahmed94 <waqasawan94@gmail.com>
2021-11-14 00:23:36 +0500

    scrollup function accurate

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,14 @@
-mybutton = document.getElementsByClassName("myBtn");
-this.window.onscroll = function(){
-    scrollFunction()
-};
-function scrollFunction(){
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        mybutton.style.display ="block";
-    } else {
+ const mybutton = document.querySelector(".myBtn");
+
+ window.addEventListener('scroll', () => {
+     if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30){
+         
+     mybutton.style.display ='block';
+     }else{
         mybutton.style.display = "none";
-    }
-}
+     }
+ })
+
 function topfunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
